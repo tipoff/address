5227abba26064e60edea62b459e1e397ac660375
drewroberts <github@drewroberts.com>
2021-03-28 22:29:37 -0400

    Install tipoff authorization package

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,42 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Database\Factories\UserFactory;
 
-class User extends Authenticatable
+class User extends \Tipoff\Authorization\Models\User
 {
-    use HasFactory, Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public static function newFactory()
+    {
+        return new UserFactory();
+    }
 }
