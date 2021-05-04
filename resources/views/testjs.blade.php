e4dab0a74cd482e66ee928a0fcc0c7fde61aab9f
devjk1 <jinsik@omniadigital.io>
2021-05-04 17:45:25 -0400

    locations + pkg setup

---
+++ resources/views/testjs.blade.php
@@ -0,0 +1,23 @@
+
+@extends ('layouts.app')
+
+@section ('content')
+
+<div class="flex flex-col items-center">
+    <div class="w-1/2">
+        @include ('addresses::domestic-address-search-bar-javascript')
+    </div>
+    <form 
+        method="POST"
+        action="{{ route('submit-form') }}" 
+        class="w-1/2"
+    >
+        @csrf
+        @include ('addresses::domestic-address-search-bar-fields-javascript')
+        <button type="submit">Submit</button>
+    </form>
+</div>
+
+
+
+@endsection
